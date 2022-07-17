f00752cb8a552f39a9c8a62c1dde9d03024065b5
kongondo <kongondo@gmail.com>
2022-07-17 18:41:00 +0100

    Add hook function that amends order view gui in order to show customisation details for a product in a line item

--- ready.php
+++ ready.php
@@ -14,8 +14,14 @@ if (!defined("PROCESSWIRE")) die();
  *
  */
 
-# HOOK to amend
+# ~~~~~~~~~~ CALL HOOKS ~~~~~~~~~~
+
+# HOOK to amend the unit weight of a product variant
 $this->addHookAfter('PadloperUtilities::getProductWeight', null, 'customProductWeight');
+
+# HOOK to process custom details for a product customisation by a customer during checkout
+// @note: the method PadloperProcessOrder::orderSaved() does not do anything; it is just for hooking so can hook before or after
+$this->addHookBefore('PadloperProcessOrder::orderSaved', null, 'processOrderProductsCustomisation');
 
 
 
@@ -55,5 +61,76 @@ function customProductWeight(HookEvent $event) {
   // set the modified value back to the return value
   // PadloperUtilities::getOrderWeight will use this new value to calculate this variants weight
   // it will take care of computing its total weight based on product quantity
+  $event->return = $value;
+}
+
+/**
+ * Hook that processes, cleans and saves product customisation details for a line item.
+ *
+ * @param HookEvent $event
+ * @return void
+ */
+function processOrderProductsCustomisation(HookEvent $event) {
+  // we need the input to get the product customisation details
+  // @note: in this example, we assume only a single customisation per line item; i.e. line item quantity not taken into consideration
+  $input = $event->input;
+
+  // Retrieve argument by name
+  /** @var PageArray $orderLineItemsPages */
+  $orderLineItemsPages = $event->arguments('orderLineItemsPages');
+  /** @var WireArray $orderLineItems */
+  $orderLineItems =  $event->arguments('orderLineItems');
+
+  ##############
+  $padloper = wire('padloper');
+  # LOOP THROUGH LINE ITEMS TO SEE WHICH ARE CUSTOMISABLE and if customer sent custom instructions
+  // get product IDs selector in order pages
+
+  $productIDsSelector = $orderLineItems->implode('|', 'productID');
+  // GET IDs of customisable products in these order line items
+  /** @var array $productIDsAllowCustomisation */
+  $productIDsAllowCustomisation = $padloper->findRaw("id={$productIDsSelector},product_is_customisable!=''", 'id');
+  // @note: id -> order line item page ID; productID -> the id of the product this line item represents
+  // @note: there are different approaches here; can loop the line items OR the line items pages OR the product IDs that allow customisation OR the line items values with order line item page ID & product ID
+  foreach ($productIDsAllowCustomisation as $productID) {
+    // get the order line item page that corresponds to this product ID
+    $orderLineItemPage = $orderLineItemsPages->get("padloper_order_line_item.productID={$productID}");
+    if (!empty($orderLineItemPage)) {
+      // grab and sanitize the custom info sent for this line item's product
+      $customisationInfo = $input->post("product_customisation_{$productID}", 'textarea');
+      if (!empty($customisationInfo)) {
+        // set and save the 'product_customise_details' field
+        $orderLineItemPage->setAndSave('product_customise_details', $customisationInfo);
+      }
+    }
+  }
+}
+
+/**
+ * Hook that amends the markup of table rows in the table that displays line items.
+ *
+ * This is in the order view dashboard that displays line items in a table.
+ *
+ * @param HookEvent $event
+ * @return void
+ */
+function customOrderLineItemTableRow(HookEvent $event) {
+  // Retrieve argument by name
+  $page = $event->arguments('page');
+  // ---------
+  // if no custom info, nothing to do
+  $customInfo = $page->product_customise_details;
+  if (empty($customInfo)) {
+    return;
+  }
+  /** @var array $value */
+  $value = $event->return;
+  // modify the title column for this table row
+  $title = $value[0];
+  // append custom info to title
+  $title .= "<div class='mt-3'><em>{$customInfo}</em></div>";
+  // title is the first element in the array; change it
+  $value[0] = $title;
+  // set the modified value back to the return value
   $event->return = $value;
 }
