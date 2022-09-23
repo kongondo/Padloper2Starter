f25c6f74ff964477550d84d77215414ee70151e9
kongondo <kongondo@gmail.com>
2022-09-23 15:27:02 +0100

    Add demo-6 hook

--- ready.php
+++ ready.php
@@ -2,7 +2,7 @@
 
 namespace ProcessWire;
 
-# Padloper 2 Starter Site - DEMO 3
+# Padloper 2 Starter Site - DEMO 6
 
 if (!defined("PROCESSWIRE")) die();
 
@@ -18,260 +18,69 @@ if (!defined("PROCESSWIRE")) die();
 
 # ~~~~~~~~~~ CALL HOOKS ~~~~~~~~~~
 
-# HOOK to check submitted customer order checkout form for VAT information errors
-// e.g. validation of VAT number for customer
-$this->addHookAfter('PadloperProcessOrder::checkCustomOrderCustomerFormForErrors', null, 'customCheckCustomOrderCustomerFormForErrors');
-
-# HOOK to check if to apply/exempt EU Digital Goods Tax
-$this->addHookAfter('PadloperUtilities::isChargeEUDigitalGoodsTax', null, 'customIsChargeEUDigitalGoodsTax');
+# HOOK to modify initial order customer values at order checkout confirmation
+// e.g. to set values for a customer who is already logged in
+$this->addHookAfter('PadloperProcessOrder::getOrderCustomer', null, 'customGetOrderCustomer');
 
 
 
 # ~~~~~~~~~~ HOOKS FUNCTIONS ~~~~~~~~~~
 
 /**
- * Hook that checks submitted checkout form for VAT information errors.
+ * Hook that modifies initial order checkout form values.
  *
- * Specifically for customers who state that they are business customers with valid VAT numbers.
+ * It does this by modifying the initial order customer properties.
+ * This only happens if order page has not yet been saved.
+ * Useful for cases you want to pre-populate checkout form for logged in users, for instance.
  *
  * @param HookEvent $event The Hook event object.
  * @return void
  */
-function customCheckCustomOrderCustomerFormForErrors(HookEvent $event) {
-
-	// @note this method has no arguments
-	// Retrieve array of all arguments
-	// $arguments = $event->arguments();
-
+function customGetOrderCustomer(HookEvent $event) {
 	$padloper = wire('padloper');
-	$post = wire('input')->post;
-	$sanitizer = wire('sanitizer');
+	$orderPage = $padloper->getOrderPage();
 
-	// BUSINESS CUSTOMER
-	if (!empty((int)$post->is_business_customer)) {
-		// customer stated that they are a business customer with a valid VAT number
-		$isInvalidVATNumber = false;
-		// ----
-		// if business customer but VAT number empty: error
-		$customerVATNo = $sanitizer->purify($post->customer_vat_number);
-		$customerVATNo = preg_replace('/\s+/', '', $customerVATNo);
-		if (empty($customerVATNo)) {
-			$isInvalidVATNumber = true;
-		} else {
-			/** @var int $customerCountryID */
-			$customerCountryID = (int) $post->shippingAddressCountry;
-			/** @var string $customerCountry */
-			$customerCountry = getCustomerCountryByID($customerCountryID);
-			/** @var array $matchedCustomerCountry */
-			$matchedCustomerCountry = $padloper->padloperUtilities->getCountryDetails($customerCountry);
-			$customerCountryCode = null;
-			if (!empty($matchedCustomerCountry['id'])) {
-				$customerCountryCode = $matchedCustomerCountry['id'];
-			}
-
-			// check if we got a country code
-			if (empty($customerCountryCode)) {
-				// no country code match for some reason
-				$isInvalidVATNumber = true;
-			} else {
-				// if business customer but VAT number invalid: error
-				$isInvalidVATNumber = empty(isValidVATNumber($customerCountryCode, $customerVATNo));
-			}
-		}
-
-		#############
-		// SET return value
-		/** @var array $value */
-		$value = $event->return;
-
-		if ($isInvalidVATNumber) {
-			// set form error input
-			// @note: this will cause checkout to return the form to the customer to correct errors
-			$value[] = 'customer_vat_number';
-		}
-		// set the modified value back to the return value
-		$event->return = $value;
-	}
-}
-
-/**
- * Hook that checks if to apply/exempt EU Digital Goods Tax.
- *
- * Specifically for customers who state that they are business customers with valid VAT numbers.
- * Plus customers whose location country is same as vendors.
- *
- * @param HookEvent $event The Hook event object.
- * @return void
- */
-function customIsChargeEUDigitalGoodsTax(HookEvent $event) {
-
-	/* FIRST CHECK GENERAL APPLICABILITY
-	- we have an order session
-	- customer in EU
-	- shop charges EU customers digital tax
-	- product is digital
-
-	*/
-	$session = wire('session');
-	if (empty($session->get('orderId'))) {
-		// no order session; return early
+	if ($orderPage instanceof Page) {
+		// we already have an order page, hence assume customer details already created and saved with order
+		// leave early
 		return;
 	}
+	// GOOD TO SET INITIAL CUSTOMER VALUES
 
-	// get the event object to give us access to PadloperUtility methods
-	$hookObject = $event->object;
+	# *** @NOTE! YOU WILL NEED TO CODE THE LOGIC TO CHECK IF YOUR CUSTOMER IS LOGGED IN + HOW TO RETRIEVE THEIR CUSTOMER DETAILS! ***
 
-	// get product settings for this line item
-	$lineItemProductSettings = $hookObject->getOrderLineItemProductSettings();
-
-	// ----------
-	// CHECK: is A DIGITAL PRODUCT?
-	// although checked earlier in PadloperUtilities, we just check again
-	if (empty($lineItemProductSettings['data']) || $lineItemProductSettings['data'] !== 'digital') {
-		// INVALID: NOT A DIGITAL PRODUCT
-		return;
-	}
-
-	// ----------
-	// CHECK: shop's policy is to charge EU customers EU digital goods vat taxes?
-	if (empty($hookObject->isShopChargingEUDigitalGoodsVATTaxes())) {
-		// INVALID: NOT CHARGING EU CUSTOMER DIGITAL TAX
-		return;
-	}
-
-	// ----------
-	// CHECK: is customer in the EU?
-	if (empty($hookObject->isOrderCustomerShippingAddressInTheEU())) {
-		// INVALID: CUSTOMER NOT IN THE EU
-		return;
-	}
-
-
-	// +++++++++++++++++++++
-	# ***** GOOD TO GO *****
-
-	$post = wire('input')->post;
-	$sanitizer = wire('sanitizer');
-	// ------
-	// @NOTE: HERE WE MIGHT NOT HAVE THIS! SO, USE POST INSTEAD!
-	// $currentOrderCustomer = $padloper->getOrderCustomer();
-	// bd($currentOrderCustomer, __METHOD__ . ': $currentOrderCustomer - at line #' . __LINE__);
-
-	// @note ->  we determine this programmatically
-	$customerVATNo = $sanitizer->purify($post->customer_vat_number);
-	$customerVATNo = preg_replace('/\s+/', '', $customerVATNo);
-	// ---------
-	/** @var int $customerCountryID */
-	$customerCountryID = (int) $post->shippingAddressCountry;
-	/** @var string $customerCountry */
-	$customerCountry = getCustomerCountryByID($customerCountryID);
-	/** @var array $matchedCustomerCountry */
-	$matchedCustomerCountry = $hookObject->getCountryDetails($customerCountry);
-	// -----------
-	$newValue = checkIfChargeEUDigitalTax($customerVATNo, $matchedCustomerCountry);
-
-	// SET return value
-	/** @var bool $value */
+	// get the order customer object to populate it with initial values
+	/** @var WireData $value */
 	$value = $event->return;
-	$value = $newValue;
+	# @see https://docs.kongondo.com/start/checkout/custom-customer-form.html#supported-form-inputs for supported properties
+	# in this example, we set customer email and shipping details.
+	# @note: shippingAddressCountryID is a runtime value but we set it here so country field in form can be populated
 
-	// set the modified value back to the return value
+	$pretendLoggedInCustomerDetails = [
+		"firstName" => 'Pretender',
+		"middleName" => 'Bling',
+		"lastName" => 'Fakeone',
+		"email" => 'pretend.customer@email.com',
+		"shippingAddressFirstName" => 'Pretender',
+		"shippingAddressLastName" => 'Fakeone',
+		"shippingAddressLineOne" => '235 Pretentious Street',
+		"shippingAddressCity" => 'Los Angeles',
+		"shippingAddressRegion" => 'California',
+		"shippingAddressCountry" => 'Andorra',
+		"shippingAddressPostalCode" => 'S234 BE',
+		"billingAddressFirstName" => 'MisterPretend',
+		"billingAddressLastName" => 'Fakebiz',
+		"billingAddressLineOne" => '456 Mybiz Avenue',
+		"billingAddressLineTwo" => 'Suite 13',
+		"billingAddressCity" => 'La Massana',
+		"billingAddressCountry" => 'Andorra',
+		"billingAddressPostalCode" => 'ANM45 999',
+		// ------
+		// @TODO -> you need to grab this ID programmatically from your shop's countries!
+		"shippingAddressCountryID" => 1931 # @NOTE: fake/testing ID!
+	];
+
+	// set values to return value (order customer)
+	$value->setArray($pretendLoggedInCustomerDetails);
 	$event->return = $value;
-}
-
-/**
- * Checks if to EU Digital Tax is applicable on line item.
- *
- * @param string $customerVATNo VAT Number to validate.
- * @param array $matchedCustomerCountry Customer country details.
- * @return bool $isChargeEUDigitalGoodsTax Whether to charge digital tax.
- */
-function checkIfChargeEUDigitalTax($customerVATNo, $matchedCustomerCountry) {
-
-	$padloper = wire('padloper');
-	$isChargeEUDigitalGoodsTax = false;
-
-	// @TODO MAYBE COULD ALSO CHECK EARLIER IF VATNUMBER IS EMPTY -> although affected by other conditions such as 'same country of origin'
-
-	$customerCountryCode = null;
-	if (!empty($matchedCustomerCountry['id'])) {
-		// we have a customer country code
-		$customerCountryCode = $matchedCustomerCountry['id'];
-	}
-
-	// get shop settings to grab vendor country code
-	$shopGeneralSettings = $padloper->getShopGeneralSettings();
-	// -----
-	$shopCountryCode = !empty($shopGeneralSettings['country']) ? $shopGeneralSettings['country'] : null;
-
-	$isVendorAndCustomerInSameCountry = false;
-	if (!empty($shopCountryCode) && !empty($customerCountryCode)) {
-		// we have both vendor and customer country codes
-		// let's compare them
-		$isVendorAndCustomerInSameCountry = ucwords($shopCountryCode) === ucwords($customerCountryCode);
-	}
-	// VENDOR AND CUSTOMER **NOT** IN SAME COUNTRY
-	// check validity of VAT number
-	if (empty($isVendorAndCustomerInSameCountry)) {
-		$isChargeEUDigitalGoodsTax = true;
-		// ** ONLY FOR BUSINESS CUSTOMERS **
-		$post = wire('input')->post;
-		// BUSINESS CUSTOMER
-		if (!empty((int)$post->is_business_customer)) {
-			// customer stated that they are a business customer
-			// ------
-			// CHECK IF VAT NUMBER IS VALID
-			$isValidVATNumber = isValidVATNumber($customerCountryCode, $customerVATNo);
-			if (!empty($isValidVATNumber)) {
-				// valid VAT Number
-				// ---------------
-				$isChargeEUDigitalGoodsTax = false;
-			}
-		}
-	}
-	// ------------
-	return $isChargeEUDigitalGoodsTax;
-}
-
-/**
- * Check if a VAT number is valid.
- *
- * Uses simple PHP SOAP client against EU wsdl service.
- *
- * @param string $countryCode Country to validate VAT Number against.
- * @param string $vatNumber VAT Number to validate.
- * @return boolean $isValidVATNumber Whether supplied VAT number is valid.
- */
-function isValidVATNumber($countryCode, $vatNumber) {
-	// ++++++++++
-	# @note: we don't remove country-specific prefixes and such!
-	# @todo: for production, developer needs to implement robustness
-	// ------------
-	$isValidVATNumber = false;
-	if (!empty($vatNumber)) {
-		// only check if VAT number is not empty
-		$wsdl = "http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl";
-		$client = new \SoapClient($wsdl);
-		// -----
-		/** @var stdClass $vatDetails */
-		$vatDetails = $client->checkVat(array(
-			'countryCode' => $countryCode,
-			'vatNumber' => $vatNumber
-		));
-		$isValidVATNumber = !empty($vatDetails->valid);
-	}
-
-	return $isValidVATNumber;
-}
-
-/**
- * Get customer country using country ID.
- *
- * @param int $customerCountryID Country page ID.
- * @return string|Null $customerCountry Customer country name if found, else Null
- */
-function getCustomerCountryByID($customerCountryID) {
-	$padloper = wire('padloper');
-	$customerCountry = $padloper->getRaw("id={$customerCountryID},template=country", 'title');
-	return $customerCountry;
 }
